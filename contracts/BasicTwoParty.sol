0e7375f66869109a48ba48390d8a93e60c219065
JBT <jbtobar.io@gmail.com>
2019-01-12 09:02:28 -0500

    basictwoparty

--- contracts/BasicTwoParty.sol
+++ contracts/BasicTwoParty.sol
@@ -27,6 +27,11 @@ contract BasicTwoParty {
     uint8 hashFunction,
     uint8 size
   );
+  event EntrySigned (
+    uint indexed key,
+    bool partyOne,
+    bool partyTwo
+  );
 
   constructor() public {
 
@@ -54,8 +59,44 @@ contract BasicTwoParty {
 
     }
 
-    function createSignedEntry() public {}
+    function createSignedEntry(
+      address _partyTwo,
+      bytes32 _digest,
+      uint8 _hashFunction,
+      uint8 _size
+    )
+      public
+    {
+      Entry memory entry = Entry(msg.sender,_partyTwo,_digest,_hashFunction,_size,true,false);
+      entries[counter+1] = entry;
+      counter += 1;
 
-    function sign() public {}
+      EntrySet(
+        counter,
+        _digest,
+        _hashFunction,
+        _size
+      );
+      emit EntrySigned(counter,true,false);
+    }
+
+    function sign(uint _counter)
+    public
+    {
+      if (entries[_counter].partyOne === msg.sender) {
+        require(entries[_counter].partyOneSigned !== true);
+        entries[_counter].partyOneSigned = true;
+
+        emit EntrySigned(_counter,true,entries[_counter].partyTwoSigned);
+      } else {
+        require(entries[_counter].partyTwo === msg.sender);
+        require(entries[_counter].partyTwoSigned !== true);
+        entries[_counter].partyTwoSigned = true;
+
+        emit EntrySigned(_counter,entries[_counter].partyOneSigned,true);
+      }
+    }
+
+
 
 }
