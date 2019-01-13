154bf4e903b2fc585febc5e7f5456e4dbd66ade1
JBT <jbtobar.io@gmail.com>
2019-01-12 19:31:44 -0500

    nft

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,6 +4,33 @@ import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
 import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
 
 contract MyNFT is ERC721Full, ERC721Mintable {
+
+  string public constant name = "GradientToken";
+  string public constant symbol = "GRAD";
+
+  struct Gradient {
+    string outer;
+    string inner;
+  }
+
+  Gradient[] public gradients;
+
   constructor() ERC721Full("MyNFT", "MNFT") public {
   }
+
+  function getGradient( uint _gradientId ) public view returns(string outer, string inner){
+    Gradient memory _grad = gradients[_gradientId];
+
+    outer = _grad.outer;
+    inner = _grad.inner;
+  }
+
+  function mint(string _outer, string _inner) public payable onlyOwner{
+    Gradient memory _gradient = Gradient({ outer: _outer, inner: _inner });
+    uint _gradientId = gradients.push(_gradient) - 1;
+
+    _mint(msg.sender, _gradientId);
+  }
+
+
 }
