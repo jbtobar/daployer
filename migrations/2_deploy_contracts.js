41996511ff9f3d80269a65e0bff14f1d041757f7
jbtobar <jbtobar.io@gmail.com>
2019-06-27 19:46:40 -0400

    migrations change

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-var Nifty = artifacts.require("./Nifty.sol");
-var Escrow = artifacts.require("./Escrow.sol")
+// var Nifty = artifacts.require("./Nifty.sol");
+// var Escrow = artifacts.require("./Escrow.sol")
 //var artifactor = require("truffle-artifactor");
 
 module.exports = function(deployer) {
-    deployer.deploy(Nifty),
-    deployer.deploy(Escrow)
+    // deployer.deploy(Nifty),
+    // deployer.deploy(Escrow)
 }
